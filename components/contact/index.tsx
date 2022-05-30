f4fdf706686cdb757b967ee4bbb4b5d0a1a1fb8c
Lucas Pacheco <lucaspdude@gmail.com>
2022-05-30 14:00:37 -0300

    feat(languages): Finished i18n implementation and translations

--- components/contact/index.tsx
+++ components/contact/index.tsx
@@ -1,20 +1,22 @@
 import { FunctionComponent } from "react";
+import { useTranslation } from "react-i18next";
 
 
 
 const ContactCommand: FunctionComponent = ({
 }) => {
 
+    const {t} = useTranslation();
     
     return (
         <div>
             <h3 className="text-4xl font-bold">Contact info</h3>
             <h5 className="text-2xl dark:text-broom-500 text-regal-blue-500 py-3">
-                Feel free to contact me on the channels provided bellow
+                {t('contact.ln1')}
             </h5>
             <div className="flex items-center my-3">
                 <div className="font-bold uppercase">
-                    Email:
+                {t('contact.ln2')}:
                 </div>
                 <div>
                 <a href="mailto:lucas@lpacheco.tech">lucas@lpacheco.tech</a>
@@ -22,10 +24,10 @@ const ContactCommand: FunctionComponent = ({
             </div>
             <div className="flex items-center my-3">
                 <div className="font-bold uppercase">
-                    Phone:
+                Github:
                 </div>
                 <div>
-                <a href="tel:+5527999072255">+55 (27) 99907 2255</a>
+                <a href="https://github.com/lucaspdude" target="_blank" rel="noreferrer">@lucaspdude</a>
                 </div>
             </div>
             
